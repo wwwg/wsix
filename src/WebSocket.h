0fb43d756e7cc110fd59aace5840b0d673631666
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-11-06 22:11:56 -0600

    Add template WebSocket header

---
+++ src/WebSocket.h
@@ -0,0 +1,26 @@
+#ifndef _WSIX_WEBSOCKET_H
+#define _WSIX_WEBSOCKET_H
+
+// C imports
+#include <cstdio>
+#include <cstdlib>
+#include <cerrno>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+// CXX imports
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+namespace wsix {
+	class WebSocket {
+	public:
+		WebSocket(string);
+	};
+};
+
+#endif
