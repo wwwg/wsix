bad57a3011938e4e4625fc1283fe7e061a9ae6fd
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-11-07 19:56:02 -0600

    Add write method in test

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,8 @@
 int main(int argc, char** argv) {
 	wsix::WebSocket w("echo.websocket.org", 80);
 	w.connect();
-	w.read();
+	char* test_buf = "asdfghi";
+	size_t test_siz = 8;
+	w.write(test_buf, test_siz, wsix::OpCode::binary);
 	return 0;
 }
